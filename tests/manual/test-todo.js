0bee2d2f3430bb041fb4d2c78810cb6ae1052985
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-10-26 22:08:58 -0500

    Added tests for the top-level skip and todo.

---
+++ tests/manual/test-todo.js
@@ -0,0 +1,12 @@
+import test from '../../index.js';
+
+test.todo('Simple smoke test', async t => {
+  t.pass();
+  t.fail();
+  await t.todo('Embedded test', async t => {
+    t.pass('embedded pass');
+    t.fail('embedded fail');
+  });
+  t.pass('pass after');
+  t.fail('fail after');
+});
