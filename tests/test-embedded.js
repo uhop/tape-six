c122359ba75ecf1b074417158841269aaf14b6df
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-10-26 21:27:29 -0500

    Added a test for an embedded test.

---
+++ tests/test-embedded.js
@@ -0,0 +1,10 @@
+import test from '../index.js';
+
+test('Simple smoke test', async t => {
+  t.pass();
+  t.fail();
+  await t.test('Embedded test', async t => {
+    t.pass('embedded pass');
+    t.fail('embedded fail');
+  });
+});
