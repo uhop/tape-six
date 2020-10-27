2d9f4ca0c8646357d316367ae41ded7c712b766b
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-10-26 21:14:13 -0500

    New main file.

---
+++ index.js
@@ -0,0 +1,24 @@
+import {test, getTests, clearTests, getReporter, setReporter, runTests} from './src/test.js';
+import defer from './src/defer.js';
+import State from './src/State.js';
+import TapReporter from './src/TapReporter.js';
+
+defer(async () => {
+  let reporter = getReporter();
+  if (!reporter) {
+    const tapReporter = new TapReporter({useJson: true});
+    reporter = tapReporter.report.bind(tapReporter);
+    setReporter(reporter);
+  }
+
+  const rootState = new State(null, {callback: reporter});
+
+
+  rootState.emit({type: 'test', test: 0, time: rootState.timer.now()});
+  const tests = getTests();
+  await runTests(rootState, tests);
+  clearTests();
+  rootState.emit({type: 'end', test: 0, time: rootState.timer.now(), fail: rootState.failed > 0, data: rootState});
+});
+
+export default test;
