365bec9aa91c2754fb44158ee5a839a0899cf24e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-11 00:10:06 -0600

    Fixed small problems with the time formatting.

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,7 +1,7 @@
 export const formatNumber = (n, precision = 0) => {
   const s = Number(Math.abs(n)).toFixed(precision),
     [i, f] = precision ? s.split('.') : [s];
-  if (i.length <= 3) return n < 0 ? '-' + s : s;
+  if (!f && i.length <= 3) return n < 0 ? '-' + s : s;
   const parts = [];
   let start = i.length % 3;
   start && parts.push(i.substr(0, start));
@@ -16,15 +16,17 @@ const SEC = 1000,
   HOUR = 60 * MIN,
   DAY = 24 * HOUR;
 
+const omit = (value, suffix, expected = '0') => (value === expected ? '' : value + suffix);
+
 export const formatTime = ms => {
   if (ms < 10) return formatNumber(ms, 3) + 'ms';
   if (ms < 100) return formatNumber(ms, 2) + 'ms';
   if (ms < SEC) return formatNumber(ms, 1) + 'ms';
   if (ms < 10000) return formatNumber(ms / SEC, 3) + 's';
   if (ms < MIN) return formatNumber(ms / SEC, 2) + 's';
-  if (ms < HOUR) return formatNumber(Math.floor(ms / MIN), 0) + 'm' + formatNumber(Math.floor((ms % MIN) / SEC), 0) + 's';
-  if (ms < DAY) return formatNumber(Math.floor(ms / HOUR), 0) + 'h' + formatNumber(Math.floor((ms % HOUR) / MIN), 0) + 'm';
-  return formatNumber(Math.floor(ms / DAY), 0) + 'd' + formatNumber(Math.floor((ms % DAY) / HOUR), 0) + 'h';
+  if (ms < HOUR) return formatNumber(Math.floor(ms / MIN), 0) + 'm' + omit(formatNumber(Math.floor((ms % MIN) / SEC), 0), 's');
+  if (ms < DAY) return formatNumber(Math.floor(ms / HOUR), 0) + 'h' + omit(formatNumber(Math.floor((ms % HOUR) / MIN), 0), 'm');
+  return formatNumber(Math.floor(ms / DAY), 0) + 'd' + omit(formatNumber(Math.floor((ms % DAY) / HOUR), 0), 'h');
 };
 
 export const formatValue = (value, skipJson) => {
