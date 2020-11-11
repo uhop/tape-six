d9e031346249e3c5a0d857cc6ad59d40b55fdf43
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-10 21:47:46 -0600

    Added failOnce option.

--- src/State.js
+++ src/State.js
@@ -1,12 +1,15 @@
 import {getTimer} from './timer.js';
 
+export class StopTest extends Error {}
+
 class State {
-  constructor(parent, {callback, skip, todo}) {
+  constructor(parent, {callback, skip, todo, failOnce}) {
     this.parent = parent;
     parent = parent || {};
     this.callback = callback || parent.callback;
     this.skip = skip || parent.skip;
     this.todo = todo || parent.todo;
+    this.failOnce = failOnce || parent.failOnce;
     this.offset = parent.asserts || 0;
     this.timer = parent.timer || getTimer();
     this.asserts = this.skipped = this.failed = 0;
@@ -21,15 +24,16 @@ class State {
   }
 
   emit(event) {
-    const originalEvent = event;
     event = {...event, skip: event.skip || this.skip, todo: event.todo || this.todo};
     !event.type && (event.type = 'assert');
+
+    const isFailed = event.fail && !event.todo && !event.skip;
 
     switch (event.type) {
       case 'assert':
         ++this.asserts;
         event.skip && ++this.skipped;
-        event.fail && !event.todo && !event.skip && ++this.failed;
+        isFailed && ++this.failed;
         event.id = this.asserts + this.offset;
         event.diffTime = event.time - this.time;
         break;
@@ -51,6 +55,10 @@ class State {
     this.callback(event);
 
     if (event.type === 'assert') {
+      if (isFailed && this.failOnce && !this.skip) {
+        for (let state = this; state; state = state.parent) state.skip = true;
+        throw new StopTest('failOnce is activated');
+      }
       this.time = this.timer.now();
     }
   }
