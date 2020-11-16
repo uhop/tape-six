c9ac33bbcae37ca7d9c80d541d06256eeb2e6dfb
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-15 18:57:07 -0600

    Removed commented-out unused code.

--- web/DomReporter.js
+++ web/DomReporter.js
@@ -27,21 +27,7 @@ class DomReporter {
           this.current.classList.remove('running');
           this.current.classList.add(event.fail ? 'failed' : 'passed');
           this.current = this.stack.pop();
-          break;
         }
-        // summary
-
-        // event.name && this.write('# finish: ' + event.name + ' # time=' + event.diffTime.toFixed(3) + 'ms', 'info');
-        // if (this.depth) break;
-        // const state = event.data,
-        //   success = state.asserts - state.failed - state.skipped;
-        // this.write('1..' + state.asserts, 'summary');
-        // this.write('# tests ' + state.asserts, 'summary');
-        // state.skipped && this.write('# skip  ' + state.skipped, 'summary-info');
-        // success && this.write('# pass  ' + success, 'summary-success');
-        // state.failed && this.write('# fail  ' + state.failed, 'summary-failure');
-        // this.write('# ' + (event.fail ? 'not ok' : 'ok'), event.fail ? 'summary-result-failure' : 'summary-result-success');
-        // this.write('# time=' + event.diffTime.toFixed(3) + 'ms', 'summary-info');
         break;
       case 'comment':
         {
@@ -143,57 +129,6 @@ class DomReporter {
           }
           this.current.appendChild(assert);
         }
-        // text = (event.fail ? 'not ok' : 'ok') + ' ' + (this.renumberAsserts ? ++this.assertCounter : event.id);
-        // if (event.skip) {
-        //   text += ' # SKIP';
-        // } else if (event.todo) {
-        //   text += ' # TODO';
-        // }
-        // event.name && (text += ' ' + event.name);
-        // text += ' # time=' + event.diffTime.toFixed(3) + 'ms';
-        // this.write(text, event.fail ? 'failure' : 'success');
-        // if (event.fail) {
-        //   this.write('  ---', 'yaml');
-        //   if (this.useJson) {
-        //     this.write('  operator: ' + event.operator, 'yaml');
-        //     if (event.data && typeof event.data == 'object') {
-        //       if (event.data.hasOwnProperty('expected')) {
-        //         try {
-        //           this.write('  expected: ' + JSON.stringify(event.data.expected), 'yaml');
-        //         } catch (error) {
-        //           // squelch
-        //         }
-        //       }
-        //       if (event.data.hasOwnProperty('actual')) {
-        //         try {
-        //           this.write('  actual:   ' + JSON.stringify(event.data.actual), 'yaml');
-        //         } catch (error) {
-        //           // squelch
-        //         }
-        //       }
-        //     }
-        //     event.at && this.write('  at: ' + event.at, 'yaml');
-        //   } else {
-        //     yamlFormatter({operator: event.operator}, formatterOptions).forEach(line => this.write(line, 'yaml'));
-        //     if (event.data) {
-        //       yamlFormatter(
-        //         {
-        //           expected: event.data.expected,
-        //           actual: event.data.actual
-        //         },
-        //         formatterOptions
-        //       ).forEach(line => this.write(line, 'yaml'));
-        //     }
-        //     yamlFormatter({at: event.at}, formatterOptions).forEach(line => this.write(line, 'yaml'));
-        //   }
-        //   const error = event.data && event.data.actual instanceof Error ? event.data.actual : event.marker,
-        //     stack = error && error.stack;
-        //   if (typeof stack == 'string') {
-        //     this.write('  stack: |-', 'yaml');
-        //     stack.split('\n').forEach(line => this.write('    ' + line, 'yaml'));
-        //   }
-        //   this.write('  ...', 'yaml');
-        // }
         break;
     }
   }
