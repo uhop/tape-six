928eac7e37f07b87e86c5ae845d48a1690f3a15d
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-16 21:57:00 -0600

    Serve everything from the single root, redirect to the web app.

--- bin/tape6-server.js
+++ bin/tape6-server.js
@@ -35,11 +35,11 @@ const mimeTable = {
   isTTY = process.stdout.isTTY,
   hasColors = isTTY && process.stdout.hasColors();
 
-let webAppRoot = process.env.WEBAPP_ROOT;
-if (!webAppRoot) {
+let webAppPath = process.env.WEBAPP_PATH;
+if (!webAppPath) {
   const url = import.meta.url;
   if (!/^file:\/\//i.test(url)) throw Error('Cannot identify the location of the web application. Use WEBAPP_PATH.');
-  webAppRoot = path.join(path.dirname(url.substr(7)), '../webApp/');
+  webAppPath = path.relative(rootFolder, path.join(path.dirname(url.substr(7)), '../webApp/'));
 }
 
 // common aliases
@@ -167,18 +167,13 @@ const server = http.createServer(async (req, res) => {
   }
   if (url.pathname === '/' || url.pathname === '/index' || url.pathname === '/index.html') {
     // redirect to the web app
-    return sendRedirect(req, res, '/webApp/index.html');
+    return sendRedirect(req, res, webAppPath);
   }
 
   if (path.normalize(url.pathname).includes('..')) return bailOut(req, res, 403);
 
-  let fileName = path.join(rootFolder, url.pathname);
-  if (url.pathname.substr(0, 8) == '/webApp/') {
-    // substitute
-    fileName = path.join(webAppRoot, url.pathname.substr(8));
-  }
-
-  const ext = path.extname(fileName).toLowerCase(),
+  const fileName = path.join(rootFolder, url.pathname),
+    ext = path.extname(fileName).toLowerCase(),
     stat = await fsp.stat(fileName).catch(() => null);
   if (stat && stat.isFile()) return sendFile(req, res, fileName, ext, method === 'HEAD');
 
