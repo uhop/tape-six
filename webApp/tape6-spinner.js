0ec3d6878dcc80b48c65ef6d7ff10ec2bb050b54
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-16 21:57:25 -0600

    More work on the spinner.

--- webApp/tape6-spinner.js
+++ webApp/tape6-spinner.js
@@ -1,6 +1,9 @@
 class Tape6Spinner extends HTMLElement {
   constructor() {
     super();
+    this.nextState = '';
+    this.inTransition = false;
+    // create squares
     let child = null;
     for (let depth = this.getAttribute('depth') || 3; depth > 0; --depth) {
       const node1 = document.createElement('div');
@@ -15,6 +18,56 @@ class Tape6Spinner extends HTMLElement {
     node.className = 'square black';
     child && node.appendChild(child);
     this.appendChild(node);
+    // watch for transitions
+    this.addEventListener('transitionend', this);
+  }
+  handleEvent(event) {
+    switch (event.type) {
+      case 'transitionend':
+        if (event.target === this) {
+          this.inTransition = false;
+          if (this.nextState) {
+            if (this.classList.contains(this.nextState)) {
+              this.nextState = '';
+            } else {
+              const nextState = this.nextState;
+              this.nextState = '';
+              this[nextState]();
+              break;
+            }
+          }
+          if (this.classList.contains('hide')) {
+            this.classList.remove('hide');
+            this.classList.add('stop');
+          }
+        }
+        break;
+    }
+  }
+  show() {
+    if (this.classList.contains('show')) return this;
+    if (this.inTransition) {
+      this.nextState = 'show';
+      return this;
+    }
+    this.style.display = 'block';
+    this.classList.remove('stop');
+    this.classList.remove('hide');
+    this.classList.add('show');
+    this.inTransition = true;
+    return this;
+  }
+  hide() {
+    if (!this.classList.contains('show')) return this;
+    if (this.inTransition) {
+      this.nextState = 'hide';
+      return this;
+    }
+    this.classList.remove('stop');
+    this.classList.remove('show');
+    this.classList.add('hide');
+    this.inTransition = true;
+    return this;
   }
 }
 customElements.define('tape6-spinner', Tape6Spinner);
