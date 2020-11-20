8e99fad2d8b1e516ea7c55cbb5c12555c33a6652
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-19 22:53:01 -0600

    Handle the config.

---
+++ src/node/config.js
@@ -0,0 +1,56 @@
+import {promises as fsp} from 'fs';
+import path from 'path';
+
+import {listing, wildToRe} from './listing.js';
+import {union, exclude} from '../utils/fileSets.js';
+
+export const resolvePatterns = async (rootFolder, patterns) => {
+  let result = [];
+  for (const item of patterns) {
+    if (item.length && item[0] == '!') {
+      result = exclude(result, wildToRe(rootFolder, item.substr(1)));
+    } else {
+      result = union(result, await listing(rootFolder, item));
+    }
+  }
+  return result;
+};
+
+export const resolveTests = async (rootFolder, type, traceFn) => {
+  let cfg;
+  // check tape6.json
+  try {
+    cfg = JSON.parse(await fsp.readFile(path.join(rootFolder, 'tape6.json')));
+  } catch (error) {
+    traceFn && traceFn('Cannot read tape6.json');
+  }
+  // check package.json, "tape6" section
+  if (!cfg) {
+    try {
+      const pkg = JSON.parse(await fsp.readFile(path.join(rootFolder, 'package.json')));
+      cfg = pkg.tape6;
+    } catch (error) {
+      traceFn && traceFn('Cannot read package.json');
+    }
+  }
+  // check well-known files
+  if (!cfg) {
+    cfg = {[type]: {tests: '/tests/test-' + type + '.*js'}};
+  }
+  // determine test patterns
+  let patterns = [];
+  if (cfg[type]) {
+    if (Array.isArray(cfg[type].tests)) {
+      patterns = patterns.concat(cfg[type].tests);
+    } else if (typeof cfg[type].tests == 'string') {
+      patterns.push(cfg[type].tests);
+    }
+  }
+  if (Array.isArray(cfg.tests)) {
+    patterns = patterns.concat(cfg.tests);
+  } else if (typeof cfg.tests == 'string') {
+    patterns.push(cfg.tests);
+  }
+  // resolve patterns
+  return resolvePatterns(rootFolder, patterns);
+};
