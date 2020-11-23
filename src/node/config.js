cf22c0750af8995daacd701b5d825594e07a055a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-23 13:57:49 -0600

    Return relative file names instead of absolute ones.

--- src/node/config.js
+++ src/node/config.js
@@ -13,7 +13,7 @@ export const resolvePatterns = async (rootFolder, patterns) => {
       result = union(result, await listing(rootFolder, item));
     }
   }
-  return result;
+  return result.map(fileName => path.relative(rootFolder, fileName));
 };
 
 export const resolveTests = async (rootFolder, type, traceFn) => {
