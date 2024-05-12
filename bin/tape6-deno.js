a97b62ac93ecc58754a4ac79b63422aeba46c247
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-12 18:01:00 -0500

    Deno CLI test runner: the first cut.

---
+++ bin/tape6-deno.js
@@ -0,0 +1,126 @@
+#!/usr/bin/env -S deno run --allow-read --allow-env --ext=js
+
+import {resolveTests, resolvePatterns} from '../src/node/config.js';
+
+import {getReporter, setReporter} from '../src/test.js';
+import State from '../src/State.js';
+import TapReporter from '../src/TapReporter.js';
+import {selectTimer} from '../src/utils/timer.js';
+
+import TestWorker from '../src/deno/TestWorker.js';
+
+const options = {},
+  rootFolder = Deno.cwd();
+
+let flags = '',
+  parallel = '',
+  files = [];
+
+const config = () => {
+  const optionNames = {
+    f: 'failureOnly',
+    t: 'showTime',
+    b: 'showBanner',
+    d: 'showData',
+    o: 'failOnce',
+    n: 'showAssertNumber'
+  };
+
+  let flagIsSet = false,
+    parIsSet = false;
+
+  for (let i = 0; i < Deno.args.length; ++i) {
+    const arg = Deno.args[i];
+    if (arg == '-f' || arg == '--flags') {
+      if (++i < Deno.args.length) {
+        flags = Deno.args[i];
+        flagIsSet = true;
+      }
+      continue;
+    }
+    if (arg == '-p' || arg == '--par') {
+      if (++i < Deno.args.length) {
+        parallel = Deno.args[i];
+        parIsSet = true;
+        if (!parallel || isNaN(parallel)) {
+          parallel = '';
+          parIsSet = false;
+        }
+      }
+      continue;
+    }
+    files.push(arg);
+  }
+
+  if (!flagIsSet) {
+    flags = Deno.env.get("TAPE6_FLAGS") || flags;
+  }
+  for (let i = 0; i < flags.length; ++i) {
+    const option = flags[i].toLowerCase(),
+      name = optionNames[option];
+    if (typeof name == 'string') options[name] = option !== flags[i];
+  }
+
+  if (!parIsSet) {
+    parallel = Deno.env.get("TAPE6_PAR") || parallel;
+  }
+  if (parallel) {
+    parallel = Math.max(0, +parallel);
+    if (parallel === Infinity) parallel = 0;
+  } else {
+    parallel = 0;
+  }
+  if (!parallel) parallel = navigator.hardwareConcurrency;
+};
+
+const init = async () => {
+  let reporter = getReporter();
+  if (!reporter) {
+    if (!Deno.env.get("TAPE6_TAP")) {
+      const TTYReporter = (await import('../src/TTYReporter.js')).default,
+        ttyReporter = new TTYReporter(options);
+      ttyReporter.testCounter = -2;
+      ttyReporter.technicalDepth = 1;
+      reporter = ttyReporter.report.bind(ttyReporter);
+    }
+    if (!reporter) {
+      const tapReporter = new TapReporter({useJson: true});
+      reporter = tapReporter.report.bind(tapReporter);
+    }
+    setReporter(reporter);
+  }
+
+  if (files.length) {
+    files = await resolvePatterns(rootFolder, files);
+  } else {
+    files = await resolveTests(rootFolder, 'deno');
+  }
+};
+
+const main = async () => {
+  config();
+  await init();
+  await selectTimer();
+
+  const rootState = new State(null, {callback: getReporter(), failOnce: options.failOnce}),
+    worker = new TestWorker(event => rootState.emit(event), parallel, options);
+
+  rootState.emit({type: 'test', test: 0, time: rootState.timer.now()});
+
+  await new Promise(resolve => {
+    worker.done = () => resolve();
+    worker.execute(files);
+  });
+
+  rootState.emit({
+    type: 'end',
+    test: 0,
+    time: rootState.timer.now(),
+    fail: rootState.failed > 0,
+    data: rootState
+  });
+
+  Deno.exit(rootState.failed > 0 ? 1 : 0);
+};
+
+main().catch(error => console.error('ERROR:', error));
---
+++ src/deno/TestWorker.js
@@ -0,0 +1,80 @@
+import {StopTest} from '../State.js';
+import EventServer from '../utils/EventServer.js';
+
+const utilName = new URL('../test.js', import.meta.url),
+  baseName = new URL('../../', import.meta.url);
+
+export default class TestWorker extends EventServer {
+  constructor(reporter, numberOfTasks = navigator.hardwareConcurrency, options) {
+    super(reporter, numberOfTasks, options);
+    this.counter = 0;
+    this.idToWorker = {};
+  }
+  makeTask(fileName) {
+    const testName = new URL(fileName, baseName),
+      id = String(++this.counter),
+      worker = new Worker(
+        new URL(
+          'data:text/javascript;charset=utf-8,' +
+            encodeURIComponent(`
+              const sanitizeMsg = msg => {
+                if (msg.type !== 'end') return msg;
+                const state = {};
+                for (const [key, value] of Object.entries(msg.data)) {
+                  if (typeof value != 'number') continue;
+                  state[key] = value;
+                }
+                return {...msg, data: state};
+              };
+
+              try {
+                const {setReporter} = await import(${JSON.stringify(utilName.href)});
+                setReporter(msg => postMessage(sanitizeMsg(msg)));
+                await import(${JSON.stringify(testName.href)});
+              } catch (error) {
+                postMessage({type: 'comment', name: 'fail to load: ' + error.message, test: 0});
+                close();
+              }
+          `)
+        ),
+        {
+          type: 'module',
+          // deno: {permissions: 'inherit'}
+        }
+      );
+    this.idToWorker[id] = worker;
+    worker.addEventListener('message', event => {
+      const msg = event.data;
+      try {
+        this.report(id, msg);
+      } catch (error) {
+        if (error instanceof StopTest) {
+          console.error('# immediate StopTest:', error.message || 'StopTest is activated');
+          worker.terminate();
+          Deno.exit(1);
+        }
+        throw error;
+      }
+      if(msg.type === 'end' && msg.test === 0) {
+        this.close(id);
+        return;
+      }
+    });
+    worker.addEventListener('error', error => {
+      this.report(id, {
+        type: 'comment',
+        name: 'fail to load: ' + (error.message || 'Worker error`'),
+        test: 0
+      });
+      this.close(id);
+    });
+    return id;
+  }
+  destroyTask(id) {
+    const worker = this.idToWorker[id];
+    if (worker) {
+      worker.terminate();
+      delete this.idToWorker[id];
+    }
+  }
+}
