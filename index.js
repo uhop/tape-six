ba1f047fb125d656e8cb903d6d35b1747c5dac92
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-22 12:28:33 -0500

    Node is recognized on Bun => moving it down the `if` chain.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-import {test, getTests, clearTests, getReporter, setReporter, runTests, getConfiguredFlag} from './src/test.js';
+import {
+  test,
+  getTests,
+  clearTests,
+  getReporter,
+  setReporter,
+  runTests,
+  getConfiguredFlag
+} from './src/test.js';
 import defer from './src/utils/defer.js';
 import State from './src/State.js';
 import TapReporter from './src/TapReporter.js';
 
-const optionNames = {f: 'failureOnly', t: 'showTime', b: 'showBanner', d: 'showData', o: 'failOnce', n: 'showAssertNumber'};
+const optionNames = {
+  f: 'failureOnly',
+  t: 'showTime',
+  b: 'showBanner',
+  d: 'showData',
+  o: 'failOnce',
+  n: 'showAssertNumber'
+};
 
 defer(async () => {
   if (getConfiguredFlag()) return; // bail out => somebody else is running the show
 
-  const isNode = typeof process == 'object' && typeof process.exit == 'function',
-    isDeno = typeof Deno == 'object' && typeof Deno.exit == 'function',
+  const isNode = typeof process == 'object' && process.versions?.node,
+    isDeno = typeof Deno == 'object',
     isBun = typeof Bun == 'object',
     isBrowser = typeof window == 'object' && !!window.location,
     options = {};
@@ -20,14 +35,14 @@ defer(async () => {
     if (typeof window.__tape6_flags == 'string') {
       flags = window.__tape6_flags;
     } else if (window.location.search) {
-      flags = (new URLSearchParams(window.location.search.substring(1))).get('flags') || '';
+      flags = new URLSearchParams(window.location.search.substring(1)).get('flags') || '';
     }
-  } else if (isNode) {
-    flags = process.env.TAPE6_FLAGS || '';
   } else if (isDeno) {
     flags = Deno.env.get('TAPE6_FLAGS') || '';
   } else if (isBun) {
     flags = Bun.env.TAPE6_FLAGS || '';
+  } else if (isNode) {
+    flags = process.env.TAPE6_FLAGS || '';
   }
 
   for (let i = 0; i < flags.length; ++i) {
@@ -39,17 +54,12 @@ defer(async () => {
   let reporter = getReporter();
   if (!reporter) {
     if (isBrowser) {
-      const id = window.__tape6_id || (new URLSearchParams(window.location.search.substring(1))).get('id');
+      const id =
+        window.__tape6_id || new URLSearchParams(window.location.search.substring(1)).get('id');
       if (typeof window.__tape6_reporter == 'function') {
         reporter = event => window.__tape6_reporter(id, event);
       } else if (window.parent && typeof window.parent.__tape6_reporter == 'function') {
         reporter = event => window.parent.__tape6_reporter(id, event);
-      }
-    } else if (isNode) {
-      if (!process.env.TAPE6_TAP) {
-        const TTYReporter = (await import('./src/TTYReporter.js')).default,
-          ttyReporter = new TTYReporter(options);
-        reporter = ttyReporter.report.bind(ttyReporter);
       }
     } else if (isDeno) {
       if (!Deno.env.get('TAPE6_TAP')) {
@@ -59,6 +69,12 @@ defer(async () => {
       }
     } else if (isBun) {
       if (!Bun.env.TAPE6_TAP) {
+        const TTYReporter = (await import('./src/TTYReporter.js')).default,
+          ttyReporter = new TTYReporter(options);
+        reporter = ttyReporter.report.bind(ttyReporter);
+      }
+    } else if (isNode) {
+      if (!process.env.TAPE6_TAP) {
         const TTYReporter = (await import('./src/TTYReporter.js')).default,
           ttyReporter = new TTYReporter(options);
         reporter = ttyReporter.report.bind(ttyReporter);
@@ -81,14 +97,20 @@ defer(async () => {
     await runTests(rootState, tests);
     await new Promise(resolve => defer(resolve));
   }
-  rootState.emit({type: 'end', test: 0, time: rootState.timer.now(), fail: rootState.failed > 0, data: rootState});
+  rootState.emit({
+    type: 'end',
+    test: 0,
+    time: rootState.timer.now(),
+    fail: rootState.failed > 0,
+    data: rootState
+  });
 
-  if (isNode) {
-    !process.env.TAPE6_WORKER && process.exit(rootState.failed > 0 ? 1 : 0);
-  } else if (isDeno) {
+  if (isDeno) {
     !Deno.env.get('TAPE6_WORKER') && Deno.exit(rootState.failed > 0 ? 1 : 0);
   } else if (isBun) {
     !Bun.env.TAPE6_WORKER && process.exit(rootState.failed > 0 ? 1 : 0);
+  } else if (isNode) {
+    !process.env.TAPE6_WORKER && process.exit(rootState.failed > 0 ? 1 : 0);
   } else if (typeof __tape6_reportResults == 'function') {
     __tape6_reportResults(rootState.failed > 0 ? 'failure' : 'success');
   }
