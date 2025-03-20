f9b159d03b0f01ddc07ef6cfe2ca55f9cdf8e4e9
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-03-20 18:39:52 -0500

    Style fix.

--- bin/tape6-runner.js
+++ bin/tape6-runner.js
@@ -3,15 +3,14 @@
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 
-const requestedRuntime =
-  {
-    node: 'tape6-node.js',
-    deno: 'tape6-deno.js',
-    bun: 'tape6-bun.js',
-    server: 'tape6-server.js',
-    runner: 'tape6-runner.js',
-    main: 'tape6.js'
-  }[process.argv[2]];
+const requestedRuntime = {
+  node: 'tape6-node.js',
+  deno: 'tape6-deno.js',
+  bun: 'tape6-bun.js',
+  server: 'tape6-server.js',
+  runner: 'tape6-runner.js',
+  main: 'tape6.js'
+}[process.argv[2]];
 
 const runtime = requestedRuntime || 'tape6.js',
   url = new URL('./' + runtime, import.meta.url),
