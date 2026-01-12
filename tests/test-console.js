b1353eedbcdbd1e47d1025ba8ee7e84915383bc3
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-01-11 19:48:58 -0600

    Improved test.

--- tests/test-console.js
+++ tests/test-console.js
@@ -1,10 +1,12 @@
 import test from '../index.js';
 
 test('console test', t => {
-  console.log('#1');
+  console.log('log #1');
   t.pass();
-  console.log('#2');
-  console.log('#2a');
+  console.log('log #2');
+  console.error('error #1');
+  console.log('log #2a');
   t.ok(1 < 2);
-  console.log('#3');
+  console.log('log #3');
+  console.error('error #2');
 });
