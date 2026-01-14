d3c2ed2504d5e091dac2909c3ebe5e91eea6fe0a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-01-13 19:58:12 -0600

    Simplified.

--- src/JSONLReporter.js
+++ src/JSONLReporter.js
@@ -6,17 +6,14 @@ class JSONLReporter {
     this.assertCounter = 0;
 
     const env = getEnv({});
-    this.prefix = env.TAPE6_JSONL_PREFIX || '';
+    this.prefix = env.TAPE6_JSONL_PREFIX ? '\n' + env.TAPE6_JSONL_PREFIX : '';
   }
   report(event) {
     if (event.type === 'assert' && this.renumberAsserts) {
       event = {...event, id: ++this.assertCounter};
     }
-    if (this.prefix) {
-      console.log('\n' + this.prefix + JSON.stringify(event));
-    } else {
-      console.log(JSON.stringify(event));
-    }
+    const jsonEvent = JSON.stringify(event);
+    console.log(this.prefix ? this.prefix + jsonEvent : jsonEvent);
   }
 }
 
