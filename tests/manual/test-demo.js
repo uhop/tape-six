6c11a5734393679d8edbf4ea31f30abc6f4f8828
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-01-24 19:25:04 -0600

    Style fix.

--- tests/manual/test-demo.js
+++ tests/manual/test-demo.js
@@ -2,7 +2,9 @@
 import test from '../../index.js';
 
 test('demo', t => {
-  const a = 1, b = 2, c = 3;
+  const a = 1,
+    b = 2,
+    c = 3;
   t.ok(a === 1, 'should pass');
   t.equal(a * b, 2);
   t.deepEqual([a, b, c], [1, 2, t.any]);
