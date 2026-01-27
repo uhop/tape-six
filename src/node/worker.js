ba88a2f7b6ae8020024ed951a37c2f709a756b33
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-01-26 22:25:47 -0600

    Switched to a reporter object + report errors.

--- src/node/worker.js
+++ src/node/worker.js
@@ -1,38 +1,33 @@
 import {parentPort} from 'node:worker_threads';
 
-const sanitizeMsg = msg => {
-  if (msg.type !== 'end') return msg;
-  const state = {};
-  for (const [key, value] of Object.entries(msg.data)) {
-    if (typeof value != 'number') continue;
-    state[key] = value;
-  }
-  return {...msg, data: state};
-};
-
 const reportToParent = fileName => msg => {
-  msg = sanitizeMsg(msg);
-  if ((msg.type === 'test' || msg.type === 'end') && !msg.test && !msg.name)
+  if ((msg.type === 'test' || msg.type === 'end') && !msg.test && !msg.name) {
     msg.name = 'FILE: /' + fileName;
+  }
   parentPort.postMessage(msg);
 };
 
 parentPort.on('message', async msg => {
   try {
-    const {setReporter} = await import(new URL('test.js', msg.srcName));
-    setReporter(reportToParent(msg.fileName));
+    const [{setReporter}, {ProxyReporter}] = await Promise.all([
+      import(new URL('test.js', msg.srcName)),
+      import(new URL('./reporters/ProxyReporter.js', msg.srcName))
+    ]);
+    setReporter(new ProxyReporter({...msg.options, reportTo: reportToParent(msg.fileName)}));
     await import(msg.testName);
   } catch (error) {
     parentPort.postMessage({type: 'test', test: 0, time: 0});
     parentPort.postMessage({type: 'comment', name: 'fail to load: ' + error.message, test: 0});
     parentPort.postMessage({
-      name: 'fail',
+      name: String(error),
       test: 0,
       marker: new Error(),
       time: 0,
-      operator: 'fail',
+      operator: 'error',
       fail: true,
-      data: {expected: true, actual: false}
+      data: {
+        actual: error
+      }
     });
     parentPort.postMessage({type: 'end', test: 0, time: 0, fail: true});
   }
