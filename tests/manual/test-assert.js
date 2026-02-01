698f7e1a407d393d8d8ea6b19914f62ed307c010
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-02-01 09:01:35 -0600

    Added a manual test for the standard `assert()`.

---
+++ tests/manual/test-assert.js
@@ -0,0 +1,9 @@
+import assert from 'node:assert';
+
+import test from '../../index.js';
+
+test('Using standard assert', t => {
+  t.pass('Pass #1');
+  assert.strict.deepEqual([1], [2], "1 should be equal to 2!");
+  t.pass('Pass #2');
+});
