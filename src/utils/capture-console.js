fad05bc106a85d44c94bd58e11f8292eccefbc4d
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-02-05 14:09:55 -0600

    Added a way to specify reporters.

--- src/utils/capture-console.js
+++ src/utils/capture-console.js
@@ -3,21 +3,26 @@ import {getReporter} from '../test.js';
 
 const consoleVerbs = {log: 1, info: 1, warn: 1, error: 1};
 
-export const captureConsole = () => {
-  const console = globalThis.console;
-  globalThis.console = new Proxy(console, {
+let originalConsole = null;
+let currentReporter = null;
+
+export const captureConsole = reporter => {
+  if (reporter) currentReporter = reporter;
+  if (originalConsole) return globalThis.console;
+  originalConsole = globalThis.console;
+  globalThis.console = new Proxy(originalConsole, {
     get(target, property, receiver) {
       const prop = Reflect.get(target, property, receiver);
       if (typeof prop === 'function') {
         if (property === 'assert') {
           return (assertion, ...args) => {
             if (assertion) return;
-            const reporter = getReporter();
+            const reporter = currentReporter || getReporter();
             reporter.report({type: 'console', name: format(...args), data: {method: property}});
           };
         } else if (consoleVerbs[property] === 1) {
           return (...args) => {
-            const reporter = getReporter();
+            const reporter = currentReporter || getReporter();
             reporter.report({type: 'console', name: format(...args), data: {method: property}});
           };
         }
@@ -25,7 +30,12 @@ export const captureConsole = () => {
       return prop;
     }
   });
-  return console;
+  return originalConsole;
 };
+
+export const getOriginalConsole = () => originalConsole;
+export const getCurrentReporter = () => currentReporter;
+
+export const setCurrentReporter = reporter => (currentReporter = reporter);
 
 export default captureConsole;
